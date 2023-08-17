a6fc518778ae7793bfb290eaa217772ddced00c9
RazSherf <101274250+RazSherf@users.noreply.github.com>
2023-08-17 19:47:45 +0300

    adding the button and create account UI

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image, KeyboardAvoidingView, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image, KeyboardAvoidingView, TextInput, Pressable } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 
 const LoginScreen = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <SafeAreaView style={styles.container} >
       <View>
@@ -31,11 +35,14 @@ const LoginScreen = () => {
             />
 
             <TextInput
+              onChange={(text) => setEmail(text)}
+              value={email}
               style={{
                 color: "gray",
                 marginVertical: 10,
                 width: 300,
-                fontSize: 16
+                fontSize: 16,
+                fontSize: email ? 16 : 16
               }}
               placeholder="Enter Your Email"
             />
@@ -44,19 +51,32 @@ const LoginScreen = () => {
         <View
           style={styles.viewStyleCss}
         >
-          <AntDesign name="lock" size={24} color="black" />
+          <AntDesign name="lock" size={24} color="black" style={{ marginLeft: 8 }} />
 
           <TextInput secureTextEntry={true}
+            onChange={(text) => setPassword(text)}
+            value={password}
             style={{
               color: "gray",
               marginVertical: 10,
               width: 300,
-              fontSize: 16
+              fontSize: 16,
+              fontSize: password ? 16 : 16
             }}
-            placeholder="enter your Password"
+            placeholder="Enter Your Password"
           />
         </View>
-
+        <View style={{ marginTop: 12, flexDirection: "row", alignItems: "center", justifyContent: 'space-between' }}>
+          <Text>Keep Me Login</Text>
+          <Text style={{ color: '#007FFF', fontWeight: 500 }}>Forgot Password?</Text>
+        </View>
+        <View style={{ marginTop: 80 }} />
+        <Pressable style={styles.logButton}>
+          <Text style={{ textAlign: 'center', color: 'white', fontSize: 16, fontWeight: "bold" }} >Login</Text>
+        </Pressable>
+        <Pressable style={{marginTop:15}}>
+          <Text style={{textAlign:'center',color:'gray',fontSize:16}}>Dont have account? Sign up </Text>
+        </Pressable>
       </KeyboardAvoidingView>
     </SafeAreaView>
   )
@@ -91,6 +111,14 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderRadius: 5,
     marginTop: 30,
+  },
+  logButton: {
+    width: 200,
+    backgroundColor: '#FEBE10',
+    borderRadius: 6,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: 15
   }
 })
 
